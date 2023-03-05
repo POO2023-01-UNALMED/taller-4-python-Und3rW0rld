505fdc3b66be166d151d3da614b7cda90b5ab3bd
Santi Acevedo <117226776+Und3rW0rld@users.noreply.github.com>
2023-03-05 00:23:30 -0500

    Solucion Taller 4 python
    B)

--- classroom/asignatura.py
+++ classroom/asignatura.py
@@ -1,8 +1,8 @@
 class Asignatura:
 
-    def __init__(self, nombre, salon):
+    def __init__(self, nombre, salon = "remoto"):
         self._nombre = nombre
         self._salon = salon
 
-    # def __str__(self):
-    #     pass
+    def __str__(self):
+        return f"{self._nombre} {self._salon}"
--- classroom/grupo.py
+++ classroom/grupo.py
@@ -1,23 +1,26 @@
 from classroom.asignatura import Asignatura
 
 class Grupo:
-    grado = None
+    grado = "Grado 12"
 
-    def __init__(self, grupo="grupo ordinado", asignaturas=None, estudiantes):
+    def __init__(self, grupo="grupo predeterminado", asignaturas=[], estudiantes=[]):
         self._grupo = grupo
         self._asignaturas = asignaturas
         self.listadoAlumnos = estudiantes
 
-    def listadoAsignaturas(self, kwargs):
+    def listadoAsignaturas(self, **kwargs):
         for x in kwargs.values():
+
             self._asignaturas.append(Asignatura(x))
 
-    def agregarAlumno(self, alumno, lista=[]):
+    def agregarAlumno(self, alumno, lista = None):
+        if lista is None:
+            lista = []
         lista.append(alumno)
         self.listadoAlumnos = self.listadoAlumnos + lista
 
-    # def __str__(self):
-    #     pass
+    def __str__(self):
+         return f"Grupo de estudiantes: {self._grupo}"
 
     @ classmethod
     def asignarNombre(cls, nombre="Grado 10"):
@@ -25,8 +28,4 @@ class Grupo:
 
     @ classmethod
     def asignarNombre(cls, nombre="Grado 6"):
-        cls.grado = nombre
-
-    @ classmethod
-    def asignarNombre(cls, nombre="Grado 4"):
         cls.grado = nombre
